28e9cc744bca33042a012dda02efa318f7e62dbe
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-10-30 23:12:16 -0500

    add site-header and app-sidebar

---
+++ app/teacher/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import * as React from 'react';
+import {
+  IconHelp,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconSettings,
+  IconHome2,
+  IconFile
+} from '@tabler/icons-react';
+
+import { NavMain } from './nav-main';
+import { NavSecondary } from './nav-secondary';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from '@/components/ui/sidebar';
+
+const data = {
+  navMain: [
+    {
+      title: 'Inicio',
+      url: '#',
+      icon: IconHome2
+    },
+    {
+      title: 'Cursos',
+      url: '#',
+      icon: IconListDetails
+    },
+    {
+      title: 'Reportes',
+      url: '#',
+      icon: IconFile
+    }
+  ],
+  navSecondary: [
+    {
+      title: 'Settings',
+      url: '#',
+      icon: IconSettings
+    },
+    {
+      title: 'Get Help',
+      url: '#',
+      icon: IconHelp
+    }
+  ]
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="size-5!" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+    </Sidebar>
+  );
+}

d40a6eb6e6b03cd363cc82668c564560ab9a553f
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-11-02 11:46:06 -0500

    add types

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export type UserSchema = {
+  id: number;
+  rol: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  state: string;
+};

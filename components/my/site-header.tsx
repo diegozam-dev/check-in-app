ac844ae9cbe84b9b7d3f33725f683d25be95131f
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-11-02 11:44:06 -0500

    add site-header

---
+++ components/my/site-header.tsx
@@ -0,0 +1,27 @@
+import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import UserButton from './user-button';
+import { users } from '@/mock/data';
+import { UserSchema } from '@/lib/types';
+
+const SiteHeader = ({ usename }: { usename: string }) => {
+  const currentUser = users.find(user => user.username === usename);
+
+  return (
+    <header className="flex h-[72px] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mx-2 data-[orientation=vertical]:h-4"
+        />
+        <h1 className="text-base font-medium">Inicio</h1>
+        <div className="ml-auto flex items-center gap-2">
+          <UserButton user={currentUser as UserSchema} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default SiteHeader;

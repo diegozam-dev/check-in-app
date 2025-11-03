2dd2b0523032077437c3841544f4a0861b0499e5
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-11-03 14:58:57 -0500

    add data-table-with-search and table columns for the users page

---
+++ components/layout/table/data-table-with-search.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getFilteredRowModel
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
+import { DataTablePagination } from '@/components/layout/table/data-table-pagination';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const DataTableWithSearch = <TData, TValue>({
+  columns,
+  data
+}: DataTableProps<TData, TValue>) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel()
+  });
+
+  return (
+    <div className="pt-6">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filtrar cursos..."
+          value={(table.getColumn('course')?.getFilterValue() as string) ?? ''}
+          onChange={event =>
+            table.getColumn('course')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <Button>Hola mundo</Button>
+      </div>
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="py-4">
+        <DataTablePagination table={table} />
+      </div>
+    </div>
+  );
+};
+
+export default DataTableWithSearch;

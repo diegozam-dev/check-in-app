3fa6a37e1d2096fcf25d36447c9e4965152f9ea7
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-11-03 14:56:37 -0500

    update name of the component

--- components/layout/typography/headings.tsx
+++ components/layout/typography/headings.tsx
@@ -6,7 +6,7 @@ export const H2 = ({ label }: { label: string }) => {
   );
 };
 
-export const TypographyH3 = ({ label }: { label: string }) => {
+export const H3 = ({ label }: { label: string }) => {
   return (
     <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
       {label}

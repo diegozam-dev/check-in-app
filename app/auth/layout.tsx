d4f756b3083157b7ecf78b51a26c46d1e4925106
Diego Tecnópolis <diegozambrano.dev@gmail.com>
2025-11-05 14:08:01 -0500

    add layout for auth

---
+++ app/auth/layout.tsx
@@ -0,0 +1,10 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <h1>Hola auth layout</h1>
+      {children}
+    </>
+  );
+};
+
+export default AuthLayout;

58c763a7e1362f80d7f65be269c0fbacb8aa3840
Diego Tecnópolis <diegozambrano.dev@gmail.com>
2025-11-07 11:56:27 -0500

    add error messages and the useActionState

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import {
   Field,
@@ -7,14 +8,20 @@ import {
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import Link from 'next/link';
+import { login } from '@/actions/auth';
+import { useActionState } from 'react';
 
 const LoginPage = () => {
+  const [state, action, pending] = useActionState(login, undefined);
+
+  if (state?.message) alert(state.message);
+
   return (
     <>
       <main className="bg-background flex min-h-svh items-center justify-center p-6 md:p-10">
         <div className="w-full max-w-sm">
           <div className="flex flex-col gap-6">
-            <form>
+            <form action={action}>
               <FieldGroup>
                 <div className="flex flex-col items-center gap-1 text-center mb-3">
                   <h1 className="text-xl font-bold">Bienvenido a Check In.</h1>
@@ -24,26 +31,40 @@ const LoginPage = () => {
                 </div>
                 {/* Usuario */}
                 <Field>
-                  <FieldLabel htmlFor="email">Usuario</FieldLabel>
+                  <FieldLabel htmlFor="username">Usuario</FieldLabel>
                   <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="Ejemplo: 0999999999"
                     required
                   />
+                  {state?.errors?.username && (
+                    <span className="text-xs text-red-600">
+                      {state.errors.username}
+                    </span>
+                  )}
                 </Field>
                 {/* Contraseña */}
                 <Field>
-                  <FieldLabel htmlFor="email">Contraseña</FieldLabel>
+                  <FieldLabel htmlFor="password">Contraseña</FieldLabel>
                   <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Ejemplo: Sekiro99."
                     required
                   />
+                  {state?.errors?.password && (
+                    <span className="text-xs text-red-500">
+                      {state.errors.password[0]}
+                    </span>
+                  )}
                 </Field>
                 <Field>
-                  <Button type="submit">Ingresar</Button>
+                  <Button type="submit" disabled={pending}>
+                    Ingresar
+                  </Button>
                 </Field>
               </FieldGroup>
             </form>

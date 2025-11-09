6d79d34046a28909ef5cc66526f89429faf6f472
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-11-09 10:25:44 -0500

    verify if session exist and redirect

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,15 @@
+'use server';
+import { verifyAuth } from '@/actions/auth';
 import { GalleryVerticalEnd } from 'lucide-react';
+import { redirect } from 'next/navigation';
 
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const user = await verifyAuth();
+
+  if (user) {
+    redirect(`/${user.username}`);
+  }
+
   return (
     <>
       <header className="flex w-full h-[72px] items-center justify-center border-b border-b-gray-200 absolute top-0">

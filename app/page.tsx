5b5966c62d524f22d8ead5d1e9f668ab0bf6a3f4
Diego Tecnópolis <diegozambrano.dev@gmail.com>
2025-12-23 09:27:19 -0500

    add appSidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { AppSidebar } from '@/components/common/appSidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hola Mundo</h1>
-    </div>
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)'
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <div className="flex flex-1 flex-col"></div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
---
+++ components/common/appSidebar.tsx
@@ -0,0 +1,150 @@
+'use client';
+
+import * as React from 'react';
+import {
+  IconChartBar,
+  IconCirclePlusFilled,
+  IconDashboard,
+  IconFolder,
+  IconHelp,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconMail,
+  IconSearch,
+  IconSettings,
+  IconUsers
+} from '@tabler/icons-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarGroup,
+  SidebarGroupContent
+} from '@/components/ui/sidebar';
+import { Button } from '../ui/button';
+
+const data = {
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '#',
+      icon: IconDashboard
+    },
+    {
+      title: 'Lifecycle',
+      url: '#',
+      icon: IconListDetails
+    },
+    {
+      title: 'Analytics',
+      url: '#',
+      icon: IconChartBar
+    },
+    {
+      title: 'Projects',
+      url: '#',
+      icon: IconFolder
+    },
+    {
+      title: 'Team',
+      url: '#',
+      icon: IconUsers
+    }
+  ],
+  navSecondary: [
+    {
+      title: 'Settings',
+      url: '#',
+      icon: IconSettings
+    },
+    {
+      title: 'Get Help',
+      url: '#',
+      icon: IconHelp
+    },
+    {
+      title: 'Search',
+      url: '#',
+      icon: IconSearch
+    }
+  ]
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="size-5!" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent className="flex flex-col gap-2">
+            <SidebarMenu>
+              <SidebarMenuItem className="flex items-center gap-2">
+                <SidebarMenuButton
+                  tooltip="Quick Create"
+                  className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                >
+                  <IconCirclePlusFilled />
+                  <span>Quick Create</span>
+                </SidebarMenuButton>
+                <Button
+                  size="icon"
+                  className="size-8 group-data-[collapsible=icon]:opacity-0"
+                  variant="outline"
+                >
+                  <IconMail />
+                  <span className="sr-only">Inbox</span>
+                </Button>
+              </SidebarMenuItem>
+            </SidebarMenu>
+            <SidebarMenu>
+              {data.navMain.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup {...props} className="mt-auto">
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {data.navSecondary.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>{/* <NavUser user={data.user} /> */}</SidebarFooter>
+    </Sidebar>
+  );
+}

6b063d13be4687d85ad48db016216330c5d67b56
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-12-25 09:52:49 -0500

    add function to obtain the full date

---
+++ lib/getFullDate.ts
@@ -0,0 +1,6 @@
+export const getFullDate = (fecha: Date): string => {
+  const dia = String(fecha.getDate()).padStart(2, '0');
+  const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+  const anio = fecha.getFullYear();
+  return `${dia}/${mes}/${anio}`;
+};

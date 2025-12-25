77bffe4575398935d3fe0bfb0a3c730e4d563e90
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-12-25 09:52:08 -0500

    add headings components

---
+++ components/common/typography/h1.tsx
@@ -0,0 +1,7 @@
+export const TypographyH1 = (text: string) => {
+  return (
+    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+      {text}
+    </h1>
+  );
+};
---
+++ components/common/typography/h3.tsx
@@ -0,0 +1,7 @@
+export const TypographyH3 = (text: string) => {
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      {text}
+    </h3>
+  );
+};

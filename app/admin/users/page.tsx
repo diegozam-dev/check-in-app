27e91f11f76acf82e692aa2cfc85e575d8ce4a03
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-12-27 12:58:55 -0500

    add pages for the admin and admin/users routes

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,17 @@
+import { TypographyH2 } from '@/components/common/typography/h2';
+import { CreateUserButton } from '@/components/feature/admin/create-user-button';
+import { RegisteredUsersTable } from '@/components/feature/admin/registered-users-table/registeredUsersTable';
+
+const UsersPage = () => {
+  return (
+    <div className="mt-4 px-4 lg:px-6">
+      <div className="flex justify-between items-center">
+        <TypographyH2 text="Usuarios Registrados" />
+        <CreateUserButton />
+      </div>
+      <RegisteredUsersTable />
+    </div>
+  );
+};
+
+export default UsersPage;
